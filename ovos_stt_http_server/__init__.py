6e600466432a41df0c3d1208d139515e57bc9bb9
jarbasai <jarbasai@mailfence.com>
2022-04-14 21:32:07 +0100

    :tada:

--- ovos_stt_http_server/__init__.py
+++ ovos_stt_http_server/__init__.py
@@ -1,34 +1,59 @@
-from asgiref.sync import sync_to_async
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from flask import Flask, request
+from speech_recognition import Recognizer, AudioFile
+from tempfile import NamedTemporaryFile
+from ovos_plugin_manager.stt import load_stt_plugin
 
 
-class AsyncContext:
-    def __init__(self, model):
-        self.model = model
-        self.text = None
-        self.buffer = []
-
-    async def start_stream(self):
-        if self.model.can_stream:
-            await sync_to_async(self.model.stream_start)()
-
-    async def stream_data(self, frames):
-        self.buffer.append(frames)
-        if self.model.can_stream:
-            await sync_to_async(self.model.stream_data)(frames)
-
-    async def stop_stream(self):
-        if self.model.can_stream:
-            text = await sync_to_async(self.model.stream_stop)()
-        else:
-            # TODO AudioData object from bytes as expected by plugins
-            text = await sync_to_async(self.model.execute)(self.buffer)
-        self.buffer = []
-        return text
+models = {}
 
 
-async def handle_raw_audio(request, ctx, sample_width):
-    while True:
-        frames = await request.content.read(sample_width * 512)
-        if not frames:
-            break
-        await ctx.stream_data(frames)
+def create_app():
+    app = Flask(__name__)
+    recognizer = Recognizer()
+
+    @app.route("/stt", methods=['POST'])
+    def get_stt():
+        audio = request.data
+        lang = str(request.args.get("lang", "en-us")).lower()
+
+        engine = models.get(lang) or models.get(lang.split("-")[0])
+        with NamedTemporaryFile() as fp:
+            fp.write(audio)
+            with AudioFile(fp.name) as source:
+                audio = recognizer.record(source)  # read the entire audio_only file
+            try:
+                return engine.execute(audio, language=lang)
+            except Exception as e:
+                print(e)
+                return ""
+
+    return app
+
+
+def start_stt_server(engine, port=9666, host="0.0.0.0", lang="en"):
+    global models
+
+    engine = load_stt_plugin(engine)
+    if not engine:
+        raise ValueError(f"Failed to load STT: {engine}")
+
+    models[lang] = engine()
+
+    app = create_app()
+    app.run(port=port, use_reloader=False, host=host)
+    return app
+
+
+
--- ovos_stt_http_server/__main__.py
+++ ovos_stt_http_server/__main__.py
@@ -1,48 +1,30 @@
-from aiohttp import web
-from ovos_stt_http_server import AsyncContext, handle_raw_audio
-from ovos_plugin_manager.stt import load_stt_plugin
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import argparse
 
-routes = web.RouteTableDef()
-
-models = {}
-
-
-@routes.post('/stt')
-async def handle_stt(request):
-
-    lang = request.query.get('lang', 'en-us').lower()
-    model = models.get(lang) or models.get(lang.split("-")[0])
-
-    ctx = AsyncContext(model)
-
-    await ctx.start_stream()
-
-    await handle_raw_audio(request, ctx, 2)
-
-    text = await ctx.stop_stream()
-
-    return web.Response(text=text)
+from ovos_stt_http_server import start_stt_server
 
 
 def main():
-    global models
-    import argparse
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--engine", help="stt plugin to be used")
+    parser.add_argument("--engine", help="stt plugin to be used", required=True)
     parser.add_argument("--port", help="port number", default=8080)
     parser.add_argument("--host", help="host", default="0.0.0.0")
+    parser.add_argument("--lang", help="language", default="en")
     args = parser.parse_args()
 
-    engine = load_stt_plugin(args.engine)
-
     # TODO one model per language cli args support
-    models["en"] = engine
-
-    app = web.Application()
-    app.add_routes(routes)
-
-    web.run_app(app, host=args.host, port=args.port)
+    start_stt_server(args.engine, args.port, args.host, args.lang)
 
 
 if __name__ == '__main__':
